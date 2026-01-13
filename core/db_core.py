dbf312a9c7d81430c12e150efd53f90f4c3a1a24
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2026-01-13 21:11:37 +0530

    updated the gitignore

--- core/db_core.py
+++ core/db_core.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from models.base_model import Base 
 
 DB_URL = "sqlite:///db/output.db"
+DB_URL2 = "sqlite:///db/experiment.db"
+
 
 
 def get_db_session():
@@ -11,3 +13,9 @@ def get_db_session():
     Session = sessionmaker(bind = engine)
     session = Session()
     return session
+
+def get_db_session2():
+    engine2 = create_engine(DB_URL2, future=True)
+    Session2 = sessionmaker(bind = engine2 )
+    session2 = Session2()
+    return session2
